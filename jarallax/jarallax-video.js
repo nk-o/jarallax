95f8891a1a4163d6cf12bf88e05aaf97c5271c02
nk-o <nkdevinfo@gmail.com>
2017-08-30 23:58:37 +0300

    ohh f... fixed again "fixed video position when container is not in the left corner"

--- jarallax/jarallax-video.js
+++ jarallax/jarallax-video.js
@@ -724,7 +724,10 @@
                 mt += (imageData.image.height - h) / 2;
             }
 
-            ml += imageData.container.left;
+            // fix if parallax block in fixed position
+            if(_this.image.position === 'fixed') {
+                ml += imageData.container.left;
+            }
 
             // add video height over than need to hide controls
             if (node === 'IFRAME') {
