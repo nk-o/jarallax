0da15d73deac7a10415f7bd650c856e7ec3b23a5
Nikita <nkdevinfo@gmail.com>
2019-09-22 12:46:08 +0300

    v1.11.1

--- dist/jarallax.js
+++ dist/jarallax.js
@@ -1,6 +1,6 @@
 /*!
  * Name    : Just Another Parallax [Jarallax]
- * Version : 1.11.0
+ * Version : 1.11.1
  * Author  : nK <https://nkdev.info>
  * GitHub  : https://github.com/nk-o/jarallax
  */
@@ -582,7 +582,12 @@ var Jarallax = function () {
             }
             // check if dom element
             if (!($imgElement instanceof Element)) {
-                $imgElement = null;
+                if (self.options.imgSrc) {
+                    $imgElement = new Image();
+                    $imgElement.src = self.options.imgSrc;
+                } else {
+                    $imgElement = null;
+                }
             }
 
             if ($imgElement) {
